f2258003723cec308ac6567aef6b34b454e9d1f7
GaryNLOL <dev.garynlol@gmail.com>
2021-12-05 18:15:25 -0500

    Moved date and hour logic to a new class

--- scheduler.py
+++ scheduler.py
@@ -1,28 +1,20 @@
 
-class Task:
-    def __init__(self,data_string):
-        # data_string: string
-        # Must be in the following format:
-        # YYYY MM DD hh mm path/to/file
-        data_string = data_string.split(' ')
-        if len(data_string) != 6:
-            raise
-        self.data = {['year','month','day','hours','minutes','path'][i]:
-                     data_string[i] for i in range(6)}
-    def __str__(self):
-        return str(self.data)
-    def __eq__(self,other):
-        return self.data == other.data
-    def __gt__(self,other):
-        
-
-class TaskRunner:
-    def __init__(self,filename):
-        self.tasks = [Task(task_data_string) for task_data_string in open(filename,'r').read().split('\n')]
-    def await_next(self):
+class Scheduler:
+    def __init__(self):
+        pass
+    def add(self,path,time):
+        pass
+    def delete(self,id):
+        pass
+    def process(self,command):
         pass
 
-task_runner = TaskRunner('example.txt')
 
-while True:
-    task_runner.await_next()
+def main():
+    scheduler = Scheduler()
+    while True:
+        command = input()
+        scheduler.process(command)
+
+if __name__ == '__main__':
+    main()
